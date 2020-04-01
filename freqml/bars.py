f2f94b8ca779096362a73eea4cb7fd6a0ba1b78b
AndreiSultan <sultan.a@phystech.edu>
2020-04-01 16:09:11 +0300

    add TB

--- freqml/bars.py
+++ freqml/bars.py
@@ -3,24 +3,35 @@ import numpy as np
 @pd.api.extensions.register_dataframe_accessor("bars")
 class bars:
     def __init__(self, df):
-        self._validate(df)
-        self._df = df
-        self._shape = df.shape
-
-    @staticmethod
-    def _validate(obj):
-        # verify there is a column latitude and a column longitude
-        if 'latitude' not in obj.columns or 'longitude' not in obj.columns:
-            raise AttributeError("Must have 'latitude' and 'longitude'.")
+       self._df = df
 
     @property
-    def TB(self, m):
-        self._df = self._df[:-self._shape[0] % m]
-        df_TB = self._df.groupby(np.floor(self._df["id"]/m)).mean()
+    def _shape(self):
+        return self._df.shape
 
+    @property
+    def volume(self):
+        return self._df["amount"].sum()
+
+    def TB(self, m=100):
+        if self._shape[0] % m != 0:
+            self._df = self._df[:-(self._shape[0] % m)]
+        grouped = self._df.groupby(np.floor(self._df["id"] / 100))
+        df_TB = grouped["price"].ohlc()
+        df_TB["amount"] = grouped["amount"].sum()
+        df_TB["VWAP"] = grouped["cost"].sum() / df_TB["amount"]
+        df_TB = df_TB.set_index(grouped["timestamp"].nth(0))
         return df_TB
 
-    def VB(self):
+    def VB(self, v=10000):
+        # if int(self.volume) % int(v) != 0:
+        #     self._df = self._df[:-(self.volume % v)]
+        # grouped = self._df.groupby(np.floor(self._df["id"] / 100))
+        # df_TB = grouped["price"].ohlc()
+        # df_TB["amount"] = grouped["amount"].sum()
+        # df_TB["VWAP"] = grouped["cost"].sum() / df_TB["amount"]
+        # df_TB = df_TB.set_index(grouped["timestamp"].nth(0))
+        # return df_TB
         pass
 
     def DB(self):
--- freqml/download.py
+++ freqml/download.py
@@ -1,6 +1,22 @@
 import os
 import pandas as pd
 
+def get_filename(curr1, curr2="USDT", exchange="binance"):
+    return "../user_data/data/" + exchange + "/" \
+               + curr1 + "_" + curr2 + "-trades.json"
+
+
+def clear(df):
+    del df['takerOrMaker']
+    del df['fee']
+    del df['info']
+    del df['symbol']
+    del df['datetime']
+    del df['type']
+    del df['order']
+    df["id"] = df["id"] - df["id"].min()
+
+
 def load(curr1, curr2="USDT", exchange="binance", t="5m", days="1"):
     if os.path.isdir("../user_data/data/" + exchange) == False:
         os.system("cd ../user_data/data && mkdir " + exchange)
@@ -13,12 +29,18 @@ def load(curr1, curr2="USDT", exchange="binance", t="5m", days="1"):
               + " -t " + t\
               + " -v --erase --dl-trades"
     os.system(command)
+    filename = get_filename(curr1, curr2, exchange)
+    os.system("gunzip " + filename + ".gz")
+
+
+def read(curr1, curr2="USDT", exchange="binance"):
+    filename = get_filename(curr1, curr2, exchange)
+    df = pd.read_json(filename)
+    clear(df)
+    return df
 
 
 def load_read(curr1, curr2="USDT", exchange="binance", t="5m", days="50"):
-    load(curr1, curr2="USDT", exchange="binance", t="5m", days=days)
-    filename = "../user_data/data/" + exchange + "/" \
-               + curr1 + "_" + curr2 + "-trades.json"
-    os.system("gunzip " + filename)
-    df = pd.read_json(filename)
+    load(curr1, curr2, exchange, t, days)
+    df = read(curr1, curr2, exchange)
     return df
